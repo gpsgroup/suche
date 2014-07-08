4f3daaf2bafd97343a0947845b5bfeb2e9d99768
Anup pokhrel <virtualanup@gmail.com>
2014-07-08 20:24:03 +0545

    Added models for 2gram and 3gram and autocompletion cache

--- suche/linguistic/models.py
+++ suche/linguistic/models.py
@@ -1,3 +1,46 @@
-from django.db import models
+'''
+Models for linguistic classes
+Contains model for the following features:
 
-# Create your models here.
+* two gram table for auto completion
+* threee gram table for auto completion
+
+Copyright (c) 2014 by anup pokhrel
+'''
+
+from django.db import models
+from django.utils import timezone
+
+class ngramModel(models.Model):
+    '''
+    ngrammodel is the base class for 2gram and 3gram models
+    '''
+    class Meta:
+        abstract = True
+
+class TwoGram(ngramModel):
+    word1 = models.CharField(max_length = 50)
+    word2 = models.CharField(max_length = 50)
+    count = models.IntegerField(default = 0)
+    perplexity = models.FloatField(default = 0)
+
+class TriGram(ngramModel):
+    word1 = models.CharField(max_length = 50)
+    word2 = models.CharField(max_length = 50)
+    word3 = models.CharField(max_length = 50)
+    count = models.IntegerField(default = 1)
+    perplexity = models.FloatField(default = 0)
+
+class CompletionCache(models.Model):
+    '''
+    CompletionCahe is a table used for compltion of quickly popular
+    queries. For example, if some celebreties got married and the
+    query for "x marries y" got very high, then it gets stored
+    in this table. Any time user searches for the query, count gets increased
+    and last_used gets updated. This data will be used for quick completion.
+    If the query is not used for about 1 day, it is removed from this
+    table.
+    '''
+    query = models.CharField(max_length = 200)
+    count = models.IntegerField(default = 1)
+    last_used = models.DateTimeField(auto_now = True, auto_now_add = True)
--- suche/suche/settings.py
+++ suche/suche/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'crawler',
     'indexer',
+    'staticpages',
+    'frontend',
+    'linguistic',
     
 )
 
