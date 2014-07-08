3258ac2caf6e7158812e7acdb6b50124eec5af42
Anup pokhrel <virtualanup@gmail.com>
2014-07-08 21:36:44 +0545

    Added dummy homepage

--- suche/frontend/views.py
+++ suche/frontend/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpRequest
+from django.template import RequestContext, loader
+from django.core.exceptions import ObjectDoesNotExist
+from django import forms
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    '''
+    This is the search engine home page handler.
+    '''
+
+    template = loader.get_template('frontend/home.html')
+
+    context = RequestContext(request, {
+        'title': "Suche"
+    })
+    return HttpResponse(template.render(context))
