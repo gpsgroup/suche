2a6efa3a2ad2ed9f558a712637b357017168d92b
mks2064 <mks2064@gmail.com>
2014-07-31 20:20:14 +0545

    Minor Adjustments

--- suche/scripts/databaseSeed.py
+++ suche/scripts/databaseSeed.py
@@ -1,10 +1,10 @@
-from scripts.spellClass import *
+from scripts.SeedClass import *
 from linguistic.models import *
 import time
 import os
 
 def run():
- spCrct=SpellChecker(os.path.join(os.path.dirname(os.path.realpath(__file__)),'big.txt'))
+ spCrct=SeedClass(os.path.join(os.path.dirname(os.path.realpath(__file__)),'big.txt'))
  print("Running")
  for key,value in spCrct.NWORDS.items():    
     wordd=Word(word=key,count=value)
