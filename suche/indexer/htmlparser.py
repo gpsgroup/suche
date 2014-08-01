a5bd9e316dbd28447b22b23d7301c4d95df48237
Anup pokhrel <virtualanup@gmail.com>
2014-08-01 20:48:18 +0545

    Added htmlparser class

---
+++ suche/indexer/htmlparser.py
@@ -0,0 +1,22 @@
+'''
+htmlparser
+takes the raw data from the crawler and
+yields various information from the data
+
+copyright(c) 2014 by suche project
+'''
+
+class HTMLParser:
+    def set_text(self,text):
+        '''
+        sets the text to process
+        '''
+        self.text = text
+
+    def parse(self):
+        '''
+        parse the html content to extract required information
+        in required format
+        '''
+        pass
+    
