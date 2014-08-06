145527083b1cd0b426ee08de3e15236b1d156525
mks2064 <mks2064@gmail.com>
2014-08-06 17:42:18 +0545

    DB seeder using ./dbseed.sh

--- suche/scripts/databaseSeed.py
+++ suche/scripts/databaseSeed.py
@@ -5,7 +5,7 @@ import os
 
 def run():
  spCrct=SeedClass(os.path.join(os.path.dirname(os.path.realpath(__file__)),'big.txt'))
- print("Running")
+ print("Please Wait.. Seeding All the words to dictionary")
  for key,value in spCrct.NWORDS.items():    
     wordd=Word(word=key,count=value)
     wordd.save()
